9ce75168ecb409d2c4864abbf8840d7869b2de72
avoro <avoronkov10@gmail.com>
2024-09-30 14:30:38 +0300

    ready hand

--- src/hand.py
+++ src/hand.py
@@ -6,17 +6,12 @@ from src.card import Card
 class Hand:
     def __init__(self, cards:  list[Card] = []):
         if cards is None:
-            # может быть пустая рука
             cards = []
         self.cards: list[Card] = cards
 
     def __repr__(self):
         return self.save()
 
-    def __eq__(self, other):
-        if isinstance(other, str):
-            other = Hand.load(other)
-        return self.cards == other.cards
 
     def save(self) -> str:
         """Convert deck to string in '4 7 0' format."""
@@ -24,14 +19,20 @@ class Hand:
         s = ' '.join(scards)
         return s
 
+    def __eq__(self, other):
+        if isinstance(other, str):
+            other = Hand.load(other)
+        return self.cards == other.cards
+
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
     @classmethod
     def load(cls, text: str) -> typing.Self:
         """Convert string in '4 7 0' format to Deck. Return deck."""
         cards = [Card.load(s) for s in text.split()]
         return cls(cards=cards)
-
-    def add_card(self, card: Card):
-        self.cards.append(card)
 
     def remove_card(self, card: Card):
         self.cards.remove(card)
